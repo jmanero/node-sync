7ae74c2425e3bffb3419659e0ce3f1da6284fc1f
John Manero <john.manero@gmail.com>
2012-05-18 18:37:57 -0400

    Adding directory layout and default files

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+var FS = require('fs');
+var HTTP = require('http');
+var Path = require('path');
+var Util = require('util');
+
+var Watch = require('fs-watcher');
+
+var Sync = module.exports = function(options) {
+	options = options || {};
+	var self = this;
+	
+	
+};
